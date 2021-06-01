4e7ef91cd38149892ea72b145d8dab16f59e0f56
Shivashish Yadav <shivashishmca@gmail.com>
2021-06-01 23:46:10 +0530

    added purgecss ad nprogress plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -195,6 +195,30 @@ module.exports = {
             },
           },
           {
+            resolve: `gatsby-plugin-purgecss`,
+            options: {
+              printRejected: true, // Print removed selectors and processed file names
+              develop: true, // Enable while using `gatsby develop`
+              // tailwind: true, // Enable tailwindcss support
+              // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+              // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+              color: `teal`,
+              purgeCSSOptions: {
+                // https://purgecss.com/configuration.html#options
+                // safelist: ['safelist'], // Don't remove this selector
+              },
+            },
+          },
+          {
+            resolve: `gatsby-plugin-nprogress`,
+            options: {
+              // Setting a color is optional.
+              color: `tomato`,
+              // Disable the loading spinner.
+              showSpinner: false,
+            },
+          },
+          {
             resolve: `gatsby-remark-embedder`,
           },
         ],
